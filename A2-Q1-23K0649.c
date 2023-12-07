b19123b0729067c47a4bd68b912ba4a4e3f367f2
SUA0649 <145201310+SUA0649@users.noreply.github.com>
2023-12-07 12:06:00 +0500

    Progress in solving question:02 and 3

--- A2-Q1-23K0649.c
+++ A2-Q1-23K0649.c
@@ -1,8 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int a[4][4]={{3,1,1,3},{2,5,0,2},{1,4,2,1},{4,7,2,4}};
-    
-
+    printf("ID:23K-0649\tName:Shaheer Uddin Ahmed\tSection: E\n");
+    int n=0;
+    printf("Enter the size of the matrices: ");
+    scanf("%d",&n);
+    if(n%2==0){
+    int roll_no;
+    printf("\nThe size of the matrix is: %dx%d\n",n,n); // Always is a even number which is divisible by 2
+    int arr[n][n];
+    int temp[n/2][n/2];
+    printf("Enter the last 4 digits of your roll no. : ");
+    scanf("%d",&roll_no);
+    for(int i=0;i<n;i++){
+        int b=10;
+        for(int j=0;j<n;j++){
+            arr[i][j] = roll_no%b;
+            b*=10; 
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    }
+    else{
+        printf("Your matrix is not of 2 matrix. Hence it cannot be divided into 2 matrices.");
+    }
     return 0;
 }
--- A2-Q2-23K0649.c
+++ A2-Q2-23K0649.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct Roles{
+#include<time.h>
+struct Info{
     char name[30];
     char role[30];
     int Communication;
@@ -8,8 +9,16 @@ struct Roles{
     int Creativity;
 };
 int main(){
+    srand(time(NULL));
+    printf("ID: 23K-0649\tName: Shaheer Uddin Ahmed\t Section: 1-E\n\n");
+    int i=0;
     char *names[20]={"Shaheer","Matthew","Tanner","Ichinose","Hagred","Zegrid","Milo","Walter","Brian","Hammad","Yuta","Yuji","Athena","Zeus","Izzazel","Hamlet","Romeo","Juliet","Luke","Arthur"}; 
-    
+    Departments D[4];
+    for(int i=0,j=0;i<4;i++){
+        while(j<5){
+        
+    }
 
     return 0;
+}
 }
